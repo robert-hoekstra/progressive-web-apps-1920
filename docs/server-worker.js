8d49623f27278b8d4773dd73cf006428a407c5b4
Robert Hoekstra <robert.hoekstra@hva.nl>
2020-03-17 14:16:52 +0100

    service worker

---
+++ docs/server-worker.js
@@ -0,0 +1,31 @@
+const cacheName = 'cache-v1';
+const precacheResources = [
+  '/',
+  'public/stylesheets/main.css',
+];
+
+self.addEventListener('install', event => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(precacheResources);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activate event!');
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(caches.match(event.request)
+    .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
+    );
+});
